6a9217707eb8478478716de1299d6cdf240a6396
nk-on <johndoe@example.com>
2024-09-22 09:59:34 +0400

    Feat: letting user to set theme 3 using Javascript

--- src/UI.js
+++ src/UI.js
@@ -4,13 +4,23 @@ const secondTheme = document.querySelector('#second-theme');
 const thirdTheme = document.querySelector('#third-theme');
 const screen = document.querySelector('.screen');
 const keyboardContainer = document.querySelector('.keyboard-container');
-const buttons = document.querySelector('.button');
+const buttons = document.querySelectorAll('.button');
 const resetButton = document.querySelector('.reset');
 const equalsButton = document.querySelector('.Equals');
+const title = document.querySelector('.title')
 secondTheme.addEventListener('change', () => {
   body.style.background = '#E6E6E6';
   screen.classList.add('screen-theme-2');
   keyboardContainer.classList.add('keyboard-theme-2');
   resetButton.classList.add('reset-theme-2');
   equalsButton.classList.add('Equals-theme-2')
+});
+thirdTheme.addEventListener('change',()=>{
+  body.style.background = '#17062A';
+  title.style.color = '#FFE53D';
+  screen.classList.add('screen-theme-3');
+  keyboardContainer.classList.add('keyboard-theme-3');
+  buttons.forEach(button => button.classList.add('button-theme-3'));
+  resetButton.classList.add('reset-theme-3');
+  equalsButton.classList.add('Equals-theme-3');
 })
