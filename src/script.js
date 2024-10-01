30fb6d5eb264e6505ad9ae6fbddf63bf66a8ee04
nk-on <2nkoz@proton.me>
2024-09-30 21:39:46 -0700

    Commenting code and removing unneeded variables

--- src/script.js
+++ src/script.js
@@ -3,10 +3,8 @@ const delButton = document.querySelector('#DEL');
 const resetButton = document.querySelector('.reset');
 const equalsButton = document.querySelector('.Equals');
 const resultsContainer = document.querySelector('.results');
-const operators = new Set(['+', '-', '*', '/']);
 const deleteButton = document.querySelector('#DEL');
 let resultString = '';
-let number = '';
 /*## Features
 1. **Entering Two Numbers:** Users can input two numbers into the calculator.
 2. **Arithmetic Operations:** Basic arithmetic operations such as addition (+), subtraction (-), multiplication (*), and division (/) are supported.
@@ -44,20 +42,25 @@ function display() {
     const data = this.dataset;
     //Preventing User from Multiple operators
     if (data.operator && !number.length) return;
-    //Handeling
+    //Handeling case when user types decimal on keyboard
     if(data.decimal){
+      //tracking how many decimals user entered
       decimals.push(data.decimal);
+      //converting initial decimal to 0.
       if (data.decimal && number.length === 0){
         resultsContainer.textContent = resultsContainer.textContent + '0.';
         return;
       }
+      //Preventing user from entering two decimals
       if(decimals.length >= 2){
         return;
       }
       resultsContainer.textContent += data.decimal;
       return;
     }
+    //tracking number entered by user 
     number += data.num;
+    //if user entered zero and any other number converting it to floating point
     if (number[0] === '0' && number.length === 2) {
       decimals.push(data.decimal);
       if(decimals >= 2){
@@ -66,6 +69,7 @@ function display() {
       data.num = `.${data.num}`;
     }
     if (data.operator) {
+      //if user clcks operator reseting tracked number and tracked decimals
       decimals = [];
       number = '';
       if (data.operator === '=') {
