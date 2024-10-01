39ed7c21b72433cf987bf10fc821ea351ee27f7d
nk-on <2nkoz@proton.me>
2024-09-30 22:19:53 -0700

    Implementing switch container color change system

--- src/UI.js
+++ src/UI.js
@@ -10,6 +10,9 @@ const equalsButton = document.querySelector('.Equals');
 const title = document.querySelector('.title');
 const DELButton = document.querySelector('.DEL');
 const elements = document.querySelectorAll('*');
+const switchContainer = document.querySelector('.switches');
+const switchNumbers = document.querySelectorAll('.number');
+console.log(switchContainer)
 function removeStyles(){
   elements.forEach(element => element.id = '');
   buttons.forEach(button => button.classList.remove('button-theme-3'));
@@ -26,7 +29,8 @@ secondTheme.addEventListener('change', () => {
   resetButton.id = 'reset-theme-2';
   equalsButton.id = 'Equals-theme-2';
   DELButton.id = 'DEL-theme-2';
-  
+  switchContainer.id = 'switches-theme-2';
+  switchNumbers.forEach( number => number.id = 'numbers-theme-2')
 });
 thirdTheme.addEventListener('change',()=>{
   body.style.background = '#17062A';
@@ -37,4 +41,6 @@ thirdTheme.addEventListener('change',()=>{
   resetButton.id = 'reset-theme-3';
   equalsButton.id = 'Equals-theme-3';
   DELButton.id  = 'DEL-theme-3';
+  switchContainer.id = 'switches-theme-3';
+  switchNumbers.forEach( number => number.id = 'numbers-theme-3')
 })
