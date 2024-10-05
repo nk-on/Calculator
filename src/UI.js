2328f4a9bebdc87431b0e3149ba4ad3730ff4acd
nk-on <2nkoz@proton.me>
2024-10-05 16:36:48 -0700

    Refactor:attaching different themes do one body element instead of writing different classes now i have more short and concise code

--- src/UI.js
+++ src/UI.js
@@ -1,4 +1,3 @@
-const body = document.querySelector('body');
 const firstTheme = document.querySelector('#first-theme');
 const secondTheme = document.querySelector('#second-theme');
 const thirdTheme = document.querySelector('#third-theme');
@@ -12,35 +11,21 @@ const DELButton = document.querySelector('.DEL');
 const elements = document.querySelectorAll('*');
 const switchContainer = document.querySelector('.switches');
 const switchNumbers = document.querySelectorAll('.number');
-console.log(switchContainer)
+const switchButton = document.querySelector('.switch');
+const body = document.querySelector('body');
+let count = 0;
 function removeStyles(){
   elements.forEach(element => element.id = '');
   buttons.forEach(button => button.classList.remove('button-theme-3'));
 };
-firstTheme.addEventListener('change',()=>{
-  location.reload();
-})
-secondTheme.addEventListener('change', () => {
-  removeStyles();
-  body.style.background = '#E6E6E6';
-  title.style.color = '#000';
-  screen.id = 'screen-theme-2'
-  keyboardContainer.id = 'keyboard-theme-2';
-  resetButton.id = 'reset-theme-2';
-  equalsButton.id = 'Equals-theme-2';
-  DELButton.id = 'DEL-theme-2';
-  switchContainer.id = 'switches-theme-2';
-  switchNumbers.forEach( number => number.id = 'numbers-theme-2')
-});
-thirdTheme.addEventListener('change',()=>{
-  body.style.background = '#17062A';
-  title.style.color = '#FFE53D';
-  screen.id = 'screen-theme-3';
-  keyboardContainer.id = 'keyboard-theme-3';
-  buttons.forEach(button => button.classList.add('button-theme-3'));
-  resetButton.id = 'reset-theme-3';
-  equalsButton.id = 'Equals-theme-3';
-  DELButton.id  = 'DEL-theme-3';
-  switchContainer.id = 'switches-theme-3';
-  switchNumbers.forEach( number => number.id = 'numbers-theme-3')
+switchButton.addEventListener('click',()=>{
+  if(count === 0){
+    body.classList.add('theme2');
+    count++;
+  }else if(count === 1){
+    body.classList.add('theme3');
+    count++;
+  }else{
+    location.reload();
+  }
 })
